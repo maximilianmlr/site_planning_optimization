ae1e45273e97dfe8cb571eeba5b419bae72d7f1c
mexemt <59599599+mexemt@users.noreply.github.com>
2020-04-23 16:57:10 +0200

    none

--- Code/Optimaziation.py
+++ Code/Optimaziation.py
@@ -5,24 +5,24 @@ import geopy.distance
 
 # Variablendekleration
 
-
-I = ['Duisburg', 'Wuppertal', 'Essen'] #Standort
-J = ['Kunde 1', 'Kunde 2', 'Kunde 3'] #Kunden
-d = int #Nachfrage
-i = [0, 0, 0]
-f = [10, 20, 10]
-c = [2, 3 , 2]
-
 plz_nrw = pd.read_csv('C:/Users/maxim/Documents/GitHub/location_optimization/Datasets/plz_nrw.csv', encoding='unicode_escape')
 plz_nrw = pd.DataFrame(plz_nrw)
+customers = pd.read_csv('C:/Users/maxim/Documents/GitHub/location_optimization/Datasets/plz_nrw.csv', encoding='unicode_escape')
+customers = pd.DataFrame(customers)
+
 plz_nrw['open'] = 0
 plz_nrw['cost_fix'] = 0
 plz_nrw['cost_var'] = 0
 
+customers['demand'] = 0
+
+c = pd.DataFrame()
+
+
+
 
 print(plz_nrw)
-df = pd.DataFrame({'Name':I,'Eröffnet':i, 'Fixkosten':f, 'Variable Kosten':c})
+print(customers)
 
-print(df)
 
 
