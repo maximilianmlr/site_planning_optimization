6d8c9e65aef309173a4e53e396c97f37341b517b
mexemt <59599599+mexemt@users.noreply.github.com>
2020-04-23 15:45:41 +0200

    t

--- Code/Optimaziation.py
+++ Code/Optimaziation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import csv
+import geopy.distance
 
 # Variablendekleration
 
@@ -12,9 +13,14 @@ i = [0, 0, 0]
 f = [10, 20, 10]
 c = [2, 3 , 2]
 
-plz_nrw = pd.read_csv("C:/Users/maxim/iCloudDrive/Uni/WiWiMaster\4. Semester\Seminar Optimierung\plz_nrw.csv")
+plz_nrw = pd.read_csv('C:/Users/maxim/Documents/GitHub/location_optimization/Datasets/plz_nrw.csv', encoding='unicode_escape')
 plz_nrw = pd.DataFrame(plz_nrw)
-print(plz_nrw.head)
+plz_nrw['open'] = 0
+plz_nrw['cost_fix'] = 0
+plz_nrw['cost_var'] = 0
+
+
+print(plz_nrw)
 df = pd.DataFrame({'Name':I,'Eröffnet':i, 'Fixkosten':f, 'Variable Kosten':c})
 
 print(df)
