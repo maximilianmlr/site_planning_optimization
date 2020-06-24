1deae288b12606d418e4fa3818fa41595537c5ea
mexemt <59599599+mexemt@users.noreply.github.com>
2020-06-24 16:04:43 +0200

    finished

--- Code/Optimaziation.py
+++ Code/Optimaziation.py
@@ -9,7 +9,7 @@ from math import exp
 
 class adddrop:
     def __init__(self, z_old, locations, distances, customers):
-        self.I = pd.DataFrame()
+        self.I_open = pd.DataFrame()
         self.z_old = z_old
         self.z_new = z_old-1
         self.counter = 0
@@ -32,14 +32,14 @@ class adddrop:
                     self.i_low = self.index
             if self.z_new < self.z_old:
                 self.locations.loc[self.locations.index[self.i_low], 'open'] = 1
-                self.I = self.I.append(pd.DataFrame(self.distances.iloc[self.i_low,:]).T, sort=False)
-                min = self.I.min().values
+                self.I_open = self.I_open.append(pd.DataFrame(self.distances.iloc[self.i_low,:]).T, sort=False)
+                min = self.I_open.min().values
 
         return self.z_old, self.locations
 
     def drop_heuristic(self):
         self.locations['open'] = 1
-        self.I = self.distances
+        self.I_open = self.distances
         self.z_new = self.locations['fixed_costs'].sum() + sum([a * b for a, b in zip(list(self.distances.min()), list(self.customers.demand))])
         while self.z_new < self.z_old:
             self.counter += + 1
@@ -47,7 +47,7 @@ class adddrop:
             self.z_old = self.z_new
             for self.index, self.row in self.locations.loc[self.locations['open']==1].iterrows():
                 self.z_fix = self.locations.loc[self.locations['open'] == 1, 'fixed_costs'].sum() - self.row['fixed_costs'] 
-                tempmin = self.I.loc[self.I.index != self.row['plz']]
+                tempmin = self.I_open.loc[self.I_open.index != self.row['plz']]
                 tempmin = np.min(tempmin, axis=0)
                 self.z = self.z_fix + sum([a * b for a, b in zip(list(tempmin), list(self.customers.demand))])
                 if self.z < self.z_new:
@@ -55,7 +55,7 @@ class adddrop:
                     self.i_low = self.row['plz']
             if self.z_new < self.z_old:
                 self.locations.loc[self.locations['plz'] == self.i_low, 'open'] = 0
-                self.I = self.I.drop([self.i_low], axis = 0)
+                self.I_open = self.I_open.drop([self.i_low], axis = 0)
         return self.z_old, self.locations
 
 class simulated_annealing:
@@ -103,14 +103,13 @@ class simulated_annealing:
                 if self.costs < self.best_costs:
                     self.best_costs = self.costs
                     self.best = self.xe.copy()
-                    print("\rNew best costs found: ", "{:.2f}".format(self.best_costs), end='')
             else:
                 expo = exp((-(self.costs-self.initial_costs))/self.T)
                 if random.random() <= expo:
                     self.initial_costs = self.costs
                     self.solution = self.xe.copy()
             self.T = self.alpha * self.T
-            print("\rTemperature: ", self.T, end="")
+            print("\rTemperature: ", self.T, "Best costs: ", self.best_costs, end="")
         return self.best_costs, self.solution.loc[self.solution['open'] == 1]
 
 class late_acceptance:
@@ -158,7 +157,7 @@ class late_acceptance:
             v = i
             self.index = v % self.l
             k += 1
-            print("\r", k, self.best_costs, end = '')
+            print("\r", k, "Best costs: ", self.best_costs, end = '')
             if self.costs <= self.list[self.index]:
                 self.start = self.xe
                 self.list[self.index] = self.costs
@@ -192,55 +191,74 @@ distances = pd.DataFrame(distances)
 
 # Cost and Demand
 
-if fixcost == 'low'
-    locations_df = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/locations_df_low.csv', encoding='unicode_escape')
+if fixcost == 'low':
+    df_loc = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/locations_df_low.csv', encoding='unicode_escape', index_col=0)
 else:
-    locations_df = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/locations_df_high.csv', encoding='unicode_escape')
+    df_loc = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/locations_df_high.csv', encoding='unicode_escape', index_col=0)
 
 
-if demand == 'low'
-    df_cust = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/customers_df_low.csv', encoding='unicode_escape')
+if demand == 'low':
+    df_cust = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/customers_df_low.csv', encoding='unicode_escape', index_col=0)
 else:
-    df_cust = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/customers_df_high.csv', encoding='unicode_escape')
+    df_cust = pd.read_csv('https://raw.githubusercontent.com/mexemt/location_optimization/master/Datasets/customers_df_high.csv', encoding='unicode_escape', index_col=0)
 
 
 # Add/Drop Heuristik
 # Start Add-Heuristik
 
+locations_df = df_loc.copy()
+customers_df = df_cust.copy()
 heuristics = adddrop(100000000000, locations_df, distances, customers_df)
 start_time = time.time()
 print("\nStarting add heuristic calculation...")
 z, df = heuristics.add_heuristic()
-open = df.loc[df['open'] == 1]
+open = df.loc[df['open'] == 1].copy()
+sol_add = df.copy()
 print('\nGesamtkosten: ', z, '\nEröffnete Standorte: \n', open)
 print("Computation time add heuristic: ", round(time.time() - start_time, 2), "seconds.")
 
-
+locations_df = df_loc.copy()
+customers_df = df_cust.copy()
 heuristics = adddrop(100000000000, locations_df, distances, customers_df)
 start_time = time.time()
 print("\nStarting drop heuristic calculation...")
 z, df = heuristics.drop_heuristic()
-open = df.loc[df['open'] == 1]
+open = df.loc[df['open'] == 1].copy()
+sol_drop = df.copy()
 print('\nGesamtkosten: ', z, "\nEröffnete Standorte: \n", open)
 print("Computation time drop heuristic: ", round(time.time() - start_time, 2), "seconds.")
 
 
-input_sa = df
+input_sa = sol_add.copy()
 start_time = time.time()
-print("\nStarting simulated annealing optimaziation...")
-simann = simulated_annealing(input_sa, distances, customers_df, 0.999, 1, 1.5)
+print("\nStarting simulated annealing optimaziation with add dataset...")
+simann = simulated_annealing(input_sa, distances, customers_df, 0.9999, 1, 1.5)
 z, open = simann.calculate()
 print('\nGesamtkosten: ', z, '\nEröffnete Standorte: \n', open)
 print("Computation time simulated annealing optimaziation: ", round(time.time() - start_time, 2), "seconds.")
 
-
-input_la = df
+input_sa = sol_drop.copy()
 start_time = time.time()
-print("\nStarting late acceptance optimaziation...")
-lateacc = late_acceptance(input_la, distances, customers_df, 20, 10000)
+print("\nStarting simulated annealing optimaziation with drop dataset...")
+simann = simulated_annealing(input_sa, distances, customers_df, 0.9999, 1, 1.5)
+z, open = simann.calculate()
+print('\nGesamtkosten: ', z, '\nEröffnete Standorte: \n', open)
+print("Computation time simulated annealing optimaziation: ", round(time.time() - start_time, 2), "seconds.")
+
+input_la = sol_add.copy()
+start_time = time.time()
+print("\nStarting late acceptance optimaziation with add dataset...")
+lateacc = late_acceptance(input_la, distances, customers_df, 20, 20000)
 z, open = lateacc.calculate()
 print('\nGesamtkosten: ', z, '\nEröffnete Standorte: \n', open)
 print("Computation time late acceptance optimaziation: ", round(time.time() - start_time, 2), "seconds.")
 
+input_la = sol_drop.copy()
+start_time = time.time()
+print("\nStarting late acceptance optimaziation with drop dataset...")
+lateacc = late_acceptance(input_la, distances, customers_df, 20, 20000)
+z, open = lateacc.calculate()
+print('\nGesamtkosten: ', z, '\nEröffnete Standorte: \n', open)
+print("Computation time late acceptance optimaziation: ", round(time.time() - start_time, 2), "seconds.")
 
 
